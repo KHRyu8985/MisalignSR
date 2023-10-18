f81c82f3ca2100f467e3454a7581de4c65819b27
kanghyun <khryu@kist.re.kr>
2023-10-18 21:06:16 +0900

    added scripts

---
+++ scripts/kernelgan_ZSSR/run.sh
@@ -0,0 +1,3 @@
+#bin/sh
+
+python scripts/kernelgan_ZSSR/run_kernelgan_zssr.py --gt_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_HR_sub/0001_s019.png' --lr_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_LR_bicubic/X2_sub/0001_s019.png' --output_dir '/home/kanghyun/MisalignSR/notebooks/figures'
--- scripts/run_kernelgan_zssr.py
+++ scripts/run_kernelgan_zssr.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # running the code example
-# python notebooks/run_kernelgan_zssr.py --gt_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_HR_sub/0001_s019.png' --lr_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_LR_bicubic/X2_sub/0001_s019.png' --output_dir '/home/kanghyun/MisalignSR/notebooks/figures'
+# python run_kernelgan_zssr.py --gt_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_HR_sub/0001_s019.png' --lr_path '/home/kanghyun/MisalignSR/datasets/DIV2K/DIV2K_train_LR_bicubic/X2_sub/0001_s019.png' --output_dir '/home/kanghyun/MisalignSR/notebooks/figures'
 
 
 def plot_and_save(image, title, filename):
